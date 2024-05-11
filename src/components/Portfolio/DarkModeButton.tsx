f1b1e138769096da869b25bb6f415ebb8b649974
Septian <fauzy434@gmail.com>
2024-05-11 22:13:45 +0700

    fix dark mode issue

--- src/components/Portfolio/DarkModeButton.tsx
+++ src/components/Portfolio/DarkModeButton.tsx
@@ -3,9 +3,7 @@ import React, { useEffect, useState } from "react";
 import { FaMoon, FaSun } from "react-icons/fa";
 
 function DarkModeButton() {
-  const [theme, setTheme] = useState<string>(
-    localStorage.getItem("theme") || "light"
-  );
+  const [theme, setTheme] = useState<string>("light");
 
   useEffect(() => {
     if (theme === "dark") {
@@ -19,19 +17,20 @@ function DarkModeButton() {
     if (theme === "dark") {
       setTheme("light");
       document.body.classList.remove("dark");
-      localStorage.setItem("theme", "light");
     } else {
       setTheme("dark");
       document.body.classList.add("dark");
-      localStorage.setItem("theme", "dark");
     }
   };
   return (
-    <button className="size-12 bg-slate-50 dark:bg-gray-700 rounded-full p-4 shadow-xl">
+    <button
+      className="size-12 bg-slate-50 dark:bg-gray-700 rounded-full p-4 shadow-xl"
+      onClick={toggleDarkMode}
+    >
       {theme === "dark" ? (
-        <FaSun className=" text-white" onClick={toggleDarkMode} />
+        <FaSun className=" text-white" />
       ) : (
-        <FaMoon className="text-gray-700" onClick={toggleDarkMode} />
+        <FaMoon className="text-gray-700" />
       )}
     </button>
   );
